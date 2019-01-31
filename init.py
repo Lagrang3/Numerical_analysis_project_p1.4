9251cdc4ea5881f927542e82d6ba78b56be9a49a
Lagrang3 <lagrangelciencia@gmail.com>
2019-01-31 11:35:49 +0100

    Cleanup.

--- init.py
+++ init.py
@@ -14,27 +14,6 @@ y_train=arc['arr_1']
 x_test=arc['arr_2']
 y_test=arc['arr_3']
 
-
-def plot_error_dist_matrix(ldist):
-	'''
-	input: a list of distance functions.
-	output: a plot 'all_dist_err.png' with comparison of
-	the distance matrices for N=100.
-	'''
-	plt.figure()
-	x=[ 100*2**i for i in range(5) ]
-	y=[]
-	for d in ldist:
-		yd = [ dist_error(dist_matrix(n,d,x_train),y_train) for n in x  ]
-		plt.plot(x,
-			yd,
-			label=d.__name__)
-		y = y + yd
-	plt.legend()
-	err_table = np.array( y ).reshape((len(ldist),len(x)))
-	print(err_table.T)
-	plt.savefig("all_dist_err.png")
-
 def plot_dist_matrix(ldist):
 	'''
 	input: a list of distance functions.
@@ -46,23 +25,53 @@ def plot_dist_matrix(ldist):
 		plt.imshow(dm,cmap='gray_r')
 		plt.savefig(dist_f.__name__+".png")
 
-def compute_and_plot_error_dist_matrix(ldist):
+def compute_error_dist_matrix(ldist):
 	'''
 	input: a list of distance functions.
 	output: a list of values of the error of the distance matrix 
 	for N = 100,200,400,800,1600.
 	'''
+	x=[ 100*2**i for i in range(5) ]
+	ly = []
 	for dist_f in ldist:
 		start=time.perf_counter()
-		x=[ 100*2**i for i in range(5) ]
 		y = [ dist_error(dist_matrix(n,dist_f,x_train),y_train) for n in x ]
 		end=time.perf_counter()
+		ly.append(y)
 		print(dist_f.__name__,y,"( %.2f  seconds)" % (end-start))
+	
+	return x,ly
+
+def plot_error_dist_matrix(ldist,x,ly):
+	'''
+	input: 
+		- list of distance functions, 
+		- list of sizes
+		- list of errors (for each function)
+	output: a plot 'all_dist_err.png' with comparison of
+	the distance matrices for N=100.
+	'''
+	plt.figure()
+	y=[]
+	for i,d in enumerate(ldist):
+		plt.plot(x,
+			ly[i],
+			label=d.__name__)
+		y = y + ly
+	plt.legend()
+	err_table = np.array( y ).reshape((len(ldist),len(x)))
+	print(err_table.T)
+	plt.savefig("all_dist_err.png")
+
+
 
 
 
 if __name__ == '__main__':
 
-	plot_error_dist_matrix([dist_oo,dist_1,dist_2,dist_H,dist_MA])
-	plot_dist_matrix([dist_oo,dist_1,dist_2,dist_H,dist_MA])
-	compute_and_plot_error_dist_matrix([dist_oo,dist_1,dist_2,dist_H,dist_MA])
+	plot_dist_matrix(
+		[dist_oo,dist_1,dist_2,dist_H,dist_MA])
+	x,ly=compute_and_plot_error_dist_matrix(
+		[dist_oo,dist_1,dist_2,dist_H,dist_MA])
+	plot_error_dist_matrix(
+		[dist_oo,dist_1,dist_2,dist_H,dist_MA],x,ly)
--- methods.py
+++ methods.py
@@ -33,17 +33,15 @@ def dist_1(a,b):
 def dist_2(a,b):
 	t=(a-b).flatten()
 	return pow(t.dot(t),0.5)
-
-
-# OJO
-def dist_H_old(a,b):
+	
+# Assignment 6.
+def dist_H(a,b):
 	fa=a*(1.0/abs(a).sum((0,1)))
 	fb=b*(1.0/abs(b).sum((0,1)))
 	df = fb-fa
 	fpart = df.flatten()
-	dx,dy = grad(df)
-	dx=dx.flatten()
-	dy=dy.flatten()
+	dx=np.diff(df,axis=0).flatten()
+	dy=np.diff(df,axis=1).flatten()
 	return pow( fpart.dot(fpart) + dx.dot(dx) + dy.dot(dy)  ,0.5)
 
 dso=ctypes.CDLL("./libdist.so")
@@ -65,7 +63,7 @@ dso.dist_MA.restype = ctypes.c_double
 c_double_p = ctypes.POINTER(ctypes.c_double)
 
 
-def dist_H(a,b):
+def dist_H_C(a,b):
 	n=len(a)
 	ta=a.flatten().astype(np.float64)
 	tta=ta.ctypes.data_as(c_double_p)
